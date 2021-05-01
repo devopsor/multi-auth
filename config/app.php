4026ac83985fd803e151ac59d45c6eaa1e9a8925
devopsor <saekiemail@gmail.com>
2021-05-01 20:10:43 +0900

    import fortify

--- config/app.php
+++ config/app.php
@@ -14,7 +14,6 @@ return [
     */
 
     'name' => env('APP_NAME', 'Laravel'),
-
     /*
     |--------------------------------------------------------------------------
     | Application Environment
@@ -174,7 +173,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-
+        App\Providers\FortifyServiceProvider::class,
     ],
 
     /*
